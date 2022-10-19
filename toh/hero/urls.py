561c0d0a79fc128c86577a81ccc720c26b4e660f
JiyeonP <q0o0p@snu.ac.kr>
2022-10-19 20:17:39 +0900

    csrf

--- toh/hero/urls.py
+++ toh/hero/urls.py
@@ -3,6 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index),
+    path('token/', views.token, name='token'),
+    path('list/', views.hero_list, name='hero_list'),
     path('<int:id>/', views.id, name='hero_id'),
     path('<str:name>/', views.name, name='hero_name'),
     path('info/<int:id>/', views.hero_info, name='hero_info'),
