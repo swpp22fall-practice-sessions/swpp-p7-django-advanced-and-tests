bb888f9c76fab5eeec394c04e204174745a687fc
KJYoung <jykim157@snu.ac.kr>
2022-10-19 20:06:33 +0900

    Postman stuffs

--- toh/hero/urls.py
+++ toh/hero/urls.py
@@ -4,7 +4,8 @@ from . import views
 urlpatterns = [
     path('', views.hero_list),
     path('index/', views.index, name='index'),
-    path('<int:id>/', views.id, name='hero_id'),
-    path('<str:name>/', views.name, name='hero_name'),
     path('info/<int:id>/', views.hero_info, name='hero_info'),
+    path('token/', views.token, name='token')
+    # path('<int:id>/', views.id, name='hero_id'),
+    # path('<str:name>/', views.name, name='hero_name'),
 ]
