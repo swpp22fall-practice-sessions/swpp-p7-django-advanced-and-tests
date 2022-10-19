f563373178c26379b8b623f9284d8112f2e6954c
ongdyub <ongdyub>
2022-10-19 20:02:52 +0900

    pratice 7

--- toh/hero/tests.py
+++ toh/hero/tests.py
@@ -1,3 +1,31 @@
-from django.test import TestCase
+from calendar import c
+from http import client
+from django.test import TestCase, Client
+from .models import Hero
+
 
 # Create your tests here.
+class HeroTestCase(TestCase):
+    def setUp(self):
+        Hero.objects.create(name='Superman')
+        Hero.objects.create(name='Batman')
+        Hero.objects.create(name='Ironman')
+
+    def test_hero_count(self):
+        self.assertEqual(Hero.objects.all().count(),3)
+
+    def test_hero_id(self) -> None:
+        client = Client()
+        response = client.get('/hero/10/')
+
+        self.assertEqual(response.status_code, 200)
+        self.assertIn('10', response.content.decode())
+
+    def test_hero_list(self):
+        client = Client()
+        response = client.get("/hero/")
+
+        self.assertEqual(response.status_code, 200)
+        self.assertIn("1", response.content.decode())
+        self.assertEqual(response.status_code, 200)
+        self.assertIn("2", response.content.decode())
