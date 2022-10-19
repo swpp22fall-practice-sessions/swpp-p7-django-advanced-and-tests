a0c5e8fca69cf9ae344cbadd8a8e1d34b449370b
jhm1313@naver.com <44698831+jhm1313@users.noreply.github.com>
2022-10-19 19:42:48 +0900

    practice 7

--- toh/hero/urls.py
+++ toh/hero/urls.py
@@ -2,7 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.hero_list),
+    # path('', views.hero_list),
+    path('', views.index),
+    path('token/', views.token, name='token'),
+    path('list/', views.hero_list, name='hero_list'),
     path('<int:id>/', views.id, name='hero_id'),
     path('<str:name>/', views.name, name='hero_name'),
     path('info/<int:id>/', views.hero_info, name='hero_info'),
