5ae71e5a344e429b9764ee61833baa56d8d938ca
pacifiic <yellowbill@snu.ac.kr>
2022-10-19 20:05:08 +0900

    commit

--- toh/hero/tests.py
+++ toh/hero/tests.py
@@ -1,3 +1,20 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from .models import Hero
 
-# Create your tests here.
+
+class HeroTestCase(TestCase):
+    def setUp(self) -> None:
+        Hero.objects.create(name='Superman')
+        Hero.objects.create(name='Batman')
+        Hero.objects.create(name='Spiderman')
+
+    def test_hero_count(self):
+        self.assertEqual(Hero.objects.all().count(), 3)
+
+    def assertCounter(self):
+        client1 = Client()
+        response = client1.get('/hero/')
+        self.assertIn('1', response.content.decode())
+        client2 = Client()
+        response = client2.get('/hero/')
+        self.assertIn('2', response.content.decode())
