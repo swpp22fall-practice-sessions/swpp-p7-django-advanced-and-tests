10366248b8b0d39191ece04f7ae6afea58161407
SINUUUU <tjd3507@snu.ac.kr>
2022-10-19 20:07:03 +0900

    final

--- toh/toh/settings.py
+++ toh/toh/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-x*cal+4dr^5wqe&0modde7$72#ck2zxe-&e=uhfvdal6(22!o)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1','0.0.0.0', 'localhost']
+ALLOWED_HOSTS = ['0.0.0.0']
 
 
 # Application definition
