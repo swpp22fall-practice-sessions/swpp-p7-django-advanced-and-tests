fb7300b79081b09bd281679b593eb6658f22da4c
Seoyoon <seoyoon5635@gmail.com>
2022-10-19 19:56:19 +0900

    Complete practice 4

--- toh/hero/urls.py
+++ toh/hero/urls.py
@@ -4,6 +4,8 @@ from . import views
 urlpatterns = [
     path('', views.index),
     path('<int:id>/', views.id, name='hero_id'),
-    path('<str:name>/', views.name, name='hero_name'),
+    # path('<str:name>/', views.name, name='hero_name'),
+    path('list/', views.hero_list, name='hero_list'),
     path('info/<int:id>/', views.hero_info, name='hero_info'),
+    path('token/', views.token, name='token'),
 ]
