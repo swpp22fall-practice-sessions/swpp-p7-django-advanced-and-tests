af97c52e6bd4e328af0e6d2e6041e7fc4aff96ed
Yujin Im <jina0605@gmail.com>
2022-10-19 19:36:06 +0900

    Implement HeroTestCase

--- toh/hero/tests.py
+++ toh/hero/tests.py
@@ -1,3 +1,13 @@
-from django.test import TestCase
+from pydoc import cli
+from django.test import TestCase, Client
+from .models import Hero
 
-# Create your tests here.
+
+class HeroTestCase(TestCase):
+    def setUp(self) -> None:
+        Hero.objects.create(name="Superman")
+        Hero.objects.create(name="Batman")
+        Hero.objects.create(name="Ironman")
+
+    def test_hero_count(self):
+        self.assertEqual(Hero.objects.all().count(), 3)
