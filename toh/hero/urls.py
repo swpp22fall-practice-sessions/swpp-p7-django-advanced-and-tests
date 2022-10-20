dc103973d0d905362cb4c9d819a12adbd0fb8659
Young-in <daddy321@snu.ac.kr>
2022-10-20 06:36:38 +0000

    practice done

--- toh/hero/urls.py
+++ toh/hero/urls.py
@@ -2,8 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.hero_list),
+    path('', views.index),
     path('<int:id>/', views.id, name='hero_id'),
-    path('<str:name>/', views.name, name='hero_name'),
+    path('info/', views.hero_list, name='hero_list'),
     path('info/<int:id>/', views.hero_info, name='hero_info'),
+    path('token/', views.token, name='token'),
 ]
