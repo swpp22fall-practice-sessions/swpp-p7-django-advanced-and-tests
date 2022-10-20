475a251e189ecd3786ad8e651e2b54715b74542a
byoungwookan97 <byoungwookan@snu.ac.kr>
2022-10-20 20:19:51 +0900

    after test

--- toh/hero/tests.py
+++ toh/hero/tests.py
@@ -1,3 +1,27 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from .models import Hero
 
 # Create your tests here.
+
+class HeroTestCase(TestCase):
+    def setUp(self):
+        Hero.objects.create(name='Superman')
+        Hero.objects.create(name='Batman')
+        Hero.objects.create(name='Ironman')
+    
+    def test_hero_count(self):
+        self.assertEqual(Hero.objects.all().count(), 3)
+        
+    def test_hero_id(self):
+        client = Client()
+        response = client.get('/hero/10/')
+
+        self.assertEqual(response.status_code, 200)
+        self.assertIn('10', response.content.decode())
+
+    def test_hero_session(self):
+        client = Client()
+        response = client.get('/hero/')
+        self.assertIn('1', response.content.decode())
+        response = client.get('/hero/')
+        self.assertIn('2', response.content.decode())
